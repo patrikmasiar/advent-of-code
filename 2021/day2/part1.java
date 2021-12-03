354c14396813accbcf617baa93dba3a5597bb3ff
Patrik Mäsiar <masiar.patrik@gmail.com>
2021-12-03 09:42:46 +0100

    Create part1.java

---
+++ 2021/day2/part1.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        File source = new File("input.txt");
+        Pattern regex = Pattern.compile("\\d+");
+
+        Scanner reader = null;
+        try {
+            reader = new Scanner(source);
+            int horizontalPosition = 0;
+            int depthPosition = 0;
+
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                Matcher match = regex.matcher(line);
+
+                if (match.find()) {
+                    int position = Integer.parseInt(match.group(0));
+
+                    if (line.contains("forward")) {
+                        horizontalPosition += position;
+                    }
+
+                    if (line.contains("down")) {
+                        depthPosition += position;
+                    }
+
+                    if (line.contains("up")) {
+                        depthPosition -= position;
+                    }
+                }
+            }
+
+            System.out.println(horizontalPosition * depthPosition);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
